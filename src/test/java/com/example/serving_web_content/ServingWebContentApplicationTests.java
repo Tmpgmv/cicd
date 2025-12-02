0f75c9185e3080dc46512e42dddee9d3450314dd
Your Name <you@example.com>
2025-12-02 23:26:15 +0300

    ci-cd.yml changed

--- src/test/java/com/example/serving_web_content/ServingWebContentApplicationTests.java
+++ src/test/java/com/example/serving_web_content/ServingWebContentApplicationTests.java
@@ -4,18 +4,39 @@ import com.example.serving_web_content.controllers.GreetingController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ServingWebContentApplicationTests {
 
     @Autowired
-    private GreetingController controller;
+    private GreetingController greetingController;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() throws Exception {
-        assertThat(controller).isNotNull();
+    void greetingControllerExists() throws Exception {
+        assertThat(greetingController).isNotNull();
+    }
+
+
+    @Test
+    void indexHtmlShouldLoad() throws Exception {
+        mockMvc.perform(get("/index.html"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void rootShouldServeIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
     }
 
 }
